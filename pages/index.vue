fd5f5a1031a4f3f5d84e4472cf9b7f9b8b024bd4
Tona <tona@mango-byte.com>
2022-01-15 13:24:54 +0700

    improve constants varibles

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+    <v-col cols="12" sm="8" md="8">
       <v-card>
         <v-card-title class="headline"> Welcome to Minority Game </v-card-title>
         <v-card-text>
@@ -9,6 +9,7 @@
         <v-card-actions>
           <v-btn color="primary" @click="start"> March Start </v-btn>
           <v-btn color="primary" @click="cancel"> March Cancel </v-btn>
+          <v-btn color="primary" @click="join"> March Join </v-btn>
           <v-btn color="primary" @click="joinCancel"> March Join Cancel </v-btn>
         </v-card-actions>
       </v-card>
@@ -22,67 +23,108 @@ import { io, Socket } from 'socket.io-client'
 
 @Component
 export default class extends Vue {
+  // SOCKET REQUEST EVENT
+  MATCH_START = '1'
+  MATCH_CANCEL = '2'
+  MATCH_JOIN = '4'
+  MATCH_JOIN_CANCEL = '3'
+
+  // SOCKET LISTEN EVENT
+  CONNECTION = 'connect'
+  DISCONNECT = 'disconnect'
+  USER_CONNECTED = '0'
+  WAITING_USERS_LIST = '13'
+  MATCH_STARTED = '4'
+  MATCH_JOINED = '2'
+  MATCH_JOIN_CANCELLED = '3'
+  MATCH_CANCELLED = '5'
+
+  // SOCKET LISTEN ERROR
+  CONNECTION_ERROR = 'connect_error'
+  GENERAL_ERROR = 'error'
+
   socket!: Socket
+
   mounted() {
     console.log('====mounted========')
     this.socket = io('http://localhost:9000', {
       path: '/minority.game/',
       auth: {
         token:
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyand0IjoiMjpkZmQ0Mjg4MC03NTFjLTExZWMtOGU0OC1kOWMzZjhiOGU1NmUiLCJkYXRhVG9rZW4iOnsiaWQiOjIsInBsYXRmb3JtVUlEIjoiMSJ9LCJpYXQiOjE2NDIxNTI3NDEsImV4cCI6MTY0MjIzOTE0MX0.l9ui_X7Zu-_TreClQdQ0u15l9-RUhgKreLCcrHO7r1U',
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyand0IjoiMTpkYjdkNTU3MC03NWI0LTExZWMtOTg2NC0zZjVlMzFjYTZkNTkiLCJkYXRhVG9rZW4iOnsiaWQiOjEsInBsYXRmb3JtVUlEIjoiMiJ9LCJpYXQiOjE2NDIyMTgwMTcsImV4cCI6MTY0MjMwNDQxN30.U5YAImBSvkyLC7CT-Z1V6A9zGgIkIpyEa8DMABLRMsw',
       },
     })
     // client-side
-    this.socket.on('connect', () => {
+    this.socket.on(this.CONNECTION, () => {
       if (this.socket.connected) {
         console.log('The client is connected')
       }
     })
-    this.socket.on('0', (data: any) => {
+    this.socket.on(this.USER_CONNECTED, (data: any) => {
       console.log('listen user connection')
       console.log(data)
     })
-    this.socket.on('13', (data: any) => {
+    this.socket.on(this.WAITING_USERS_LIST, (data: any) => {
       console.log('waiting users list')
       console.log(data)
     })
-    this.socket.on('3', (data: any) => {
+    this.socket.on(this.MATCH_JOINED, (data: any) => {
+      console.log('matches joined')
+      console.log(data)
+    })
+    this.socket.on(this.MATCH_JOIN_CANCELLED, (data: any) => {
       console.log('matches join cancelled')
       console.log(data)
     })
-    this.socket.on('4', (data: any) => {
+    this.socket.on(this.MATCH_STARTED, (data: any) => {
       console.log('matches started')
       console.log(data)
     })
-    this.socket.on('5', (data: any) => {
+    this.socket.on(this.MATCH_CANCELLED, (data: any) => {
       console.log('matches cancelled')
       console.log(data)
     })
-    this.socket.on('disconnect', () => {
+    this.socket.on(this.DISCONNECT, () => {
       console.log('The client is disconnect')
     })
-    this.socket.on('connect_error', (err) => {
+    this.socket.on(this.CONNECTION_ERROR, (err) => {
       console.log(err)
     })
-    this.socket.on('error', (err) => {
+    this.socket.on(this.GENERAL_ERROR, (err) => {
       console.log(err)
     })
   }
 
   start() {
     console.log('matches starting...')
+    this.socket.emit(this.MATCH_START, {
+      eventId: 'abbdc8b6-d85e-425c-997e-9ffbfbb454c9',
+    })
   }
 
   cancel() {
     console.log('matches cancel...')
-    this.socket.emit('2', {
-      eventId: '26cffccf-2d77-4f99-8640-08ce63a7df93',
-      userId: 2,
+    this.socket.emit(this.MATCH_CANCEL, {
+      eventId: 'abbdc8b6-d85e-425c-997e-9ffbfbb454c9',
+      userId: 1,
+    })
+  }
+
+  join() {
+    console.log('matches join...')
+    this.socket.emit(this.MATCH_JOIN, {
+      eventId: 'abbdc8b6-d85e-425c-997e-9ffbfbb454c9',
+      matchId: 6,
     })
   }
 
   joinCancel() {
     console.log('matches join cancel...')
+    this.socket.emit(this.MATCH_JOIN_CANCEL, {
+      eventId: 'abbdc8b6-d85e-425c-997e-9ffbfbb454c9',
+      matchId: 6,
+      userId: 1,
+    })
   }
 }
 </script>
